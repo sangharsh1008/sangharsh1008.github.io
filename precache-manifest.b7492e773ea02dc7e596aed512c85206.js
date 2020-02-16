0c130f4c0064e8bbf9ec006d6c3b3bdb0a38497c
sangharsh <sangharshs@mitrmedia.com>
2020-02-16 12:53:08 +0530

    initialCommit

---
+++ precache-manifest.b7492e773ea02dc7e596aed512c85206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a38139a313b46cf9393ac7a36d5247",
+    "url": "./index.html"
+  },
+  {
+    "revision": "661d64f022ac7279c5dd",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f25a9aff6c043537a40e",
+    "url": "./static/css/main.b28fa2ba.chunk.css"
+  },
+  {
+    "revision": "661d64f022ac7279c5dd",
+    "url": "./static/js/2.9a062cc2.chunk.js"
+  },
+  {
+    "revision": "f25a9aff6c043537a40e",
+    "url": "./static/js/main.77153686.chunk.js"
+  },
+  {
+    "revision": "3e455c1c8fa4d002b0e6",
+    "url": "./static/js/runtime-main.88d1c77f.js"
+  }
+]);
